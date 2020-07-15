ca456f739d999df7008af40df1a64e56d40825e8
iGiant <sergey.grishayev@gmail.com>
2020-07-15 18:50:05 +0400

    Обработка окончаний отрицательных чисел

--- ending.go
+++ ending.go
@@ -4,6 +4,9 @@ package num2words
 // form1 - соответствует 1 шт, form2 - соответствует от 2 до 4 шт, form3 - остальные
 // например Enditive(119, "яблоко", "яблока", "яблок") вернет "яблок"
 func Enditive(num int, form1, form2, form3 string) string {
+	if num < 0 {
+		num *= -1
+	}
 	mod := num % 100
 	if mod >= 11 && mod <= 20 {
 		return form3
